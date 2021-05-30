20f243b82aae442627cfd6eee86f413468efa644
BobbleCat <treecear@hotmail.com>
2021-05-30 13:31:59 +0100

    Better Version

--- src/main/java/com/example/currency/controller/CurrencyRatesController.java
+++ src/main/java/com/example/currency/controller/CurrencyRatesController.java
@@ -3,6 +3,7 @@ package com.example.currency.controller;
 
 import com.example.currency.model.CurrencyRates;
 import com.example.currency.service.CurrencyRatesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyRatesController {
 
-    public CurrencyRatesService currencyRatesService = new CurrencyRatesService();
+    @Autowired
+    public CurrencyRatesService currencyRatesService;
 
     @GetMapping("/rates")
     public CurrencyRates currencyRates(){

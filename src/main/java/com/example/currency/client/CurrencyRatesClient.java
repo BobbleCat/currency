95f8913c86e41e10f5ed58d1c1b20b3da07d7658
BobbleCat <treecear@hotmail.com>
2021-06-06 12:45:28 +0100

    - Added Currency Rates Client - Added wiring for the client in Currency Rates Service

---
+++ src/main/java/com/example/currency/client/CurrencyRatesClient.java
@@ -0,0 +1,11 @@
+package com.example.currency.client;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CurrencyRatesClient {
+
+    public String getCurrencyRates(){
+        return "Hello";
+    }
+
+}
--- src/main/java/com/example/currency/service/CurrencyRatesService.java
+++ src/main/java/com/example/currency/service/CurrencyRatesService.java
@@ -1,13 +1,19 @@
 package com.example.currency.service;
 
+import com.example.currency.client.CurrencyRatesClient;
 import com.example.currency.model.CurrencyRates;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CurrencyRatesService {
+
+    private final CurrencyRatesClient currencyRatesClient;
 
     public CurrencyRates getCurrencyRates() {
         // Call the currency rates client
         return new CurrencyRates();
     }
+
 }

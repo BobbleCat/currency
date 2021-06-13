bfa6353dfc0e4103531755b25a543f5d34a9db5b
BobbleCat <treecear@hotmail.com>
2021-06-13 01:27:12 +0100

    - Added wiring for Currency Rates Service in Currency Rates controller

--- src/main/java/com/example/currency/controller/CurrencyRatesController.java
+++ src/main/java/com/example/currency/controller/CurrencyRatesController.java
@@ -3,6 +3,7 @@ package com.example.currency.controller;
 
 import com.example.currency.model.CurrencyRates;
 import com.example.currency.service.CurrencyRatesService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 public class CurrencyRatesController {
 
     @Autowired
-    public CurrencyRatesService currencyRatesService;
+    private final CurrencyRatesService currencyRatesService;
 
     @GetMapping("/rates")
     public CurrencyRates currencyRates(){
